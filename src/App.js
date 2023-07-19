453bb15128235c2cbb225301c7412983bfdac9e5
Elvis20-23 <kiubyes.15@gmail.com>
2023-07-18 20:07:50 -0500

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './components/C01componente';
+import C02contador from './components/C02contador';
+import C03doblecontador from './components/C03doblecontador';
+import C04variable from './components/C04variable';
+import C05operador from './components/C05operadorTernario';
+import C06matriz from './components/C06matriz1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h3>Primer componente</h3>
+      <C01componente />
+      <h3>Segundo componente</h3>
+      <C02contador />
+      <h3>Tercer componente</h3>
+      <C03doblecontador />
+      <h3>Cuarto componente</h3>
+      <C04variable xVariable="Hola mundo..." />
+      <h3>Quinto componente</h3>
+      <C05operador />
+      <h3>Sexto componente</h3>
+      <C06matriz />
+
     </div>
   );
 }
